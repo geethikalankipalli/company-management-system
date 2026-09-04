aea267e261f7c9248d8f8c337ae1ded78cc3c047
Lankipalli Geethika <geethika.lankipalli@gmail.com>
2026-09-04 14:11:57 +0530

    Remove temporary Render admin command

--- company_management/settings.py
+++ company_management/settings.py
@@ -84,13 +84,17 @@ WSGI_APPLICATION = 'company_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
